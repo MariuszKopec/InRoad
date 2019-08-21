7d523c2763350644c84cf2aef5a053d9b5c97593
mariusz.kopec <mariuszkopec88@gmail.com>
2019-08-21 21:50:08 +0200

    add ResultView

--- feature_calculator/src/main/java/com/ocado/feature/calculator/view/KeyboardView.kt
+++ feature_calculator/src/main/java/com/ocado/feature/calculator/view/KeyboardView.kt
@@ -1,11 +1,11 @@
-package com.ocado.feature.calculator.keyboard
+package com.ocado.feature.calculator.view
 
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
 import com.ocado.feature.calculator.R
 
-class KeyboardView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+internal class KeyboardView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
     LinearLayout(context, attrs, defStyleAttr) {
 
     init {
---
+++ feature_calculator/src/main/java/com/ocado/feature/calculator/view/ResultView.kt
@@ -0,0 +1,14 @@
+package com.ocado.feature.calculator.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import com.ocado.feature.calculator.R
+
+internal class ResultView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
+        LinearLayout(context, attrs, defStyleAttr) {
+
+    init {
+        inflate(context, R.layout.view_result, this)
+    }
+}
